270fbebea5572579370924f577ce5294e19a2a9f
Mare <sevilamare@gmail.com>
2020-10-20 13:16:57 -0300

    rubocop

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,8 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render  json: @users, status: :ok
+    render json: @users, status: :ok
   end
 
-  def create
-    
-  end
+  def create; end
 end
