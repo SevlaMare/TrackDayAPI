59cb0d0ffbef05e38c8da8fde32f03472877a7f7
Mare <sevilamare@gmail.com>
2020-10-20 13:14:44 -0300

    user controler index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+
+    render  json: @users, status: :ok
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :books, only: [:show, :new, :create]
+      resources :users, only: [:index, :show, :new, :create]
     end
   end
 end
