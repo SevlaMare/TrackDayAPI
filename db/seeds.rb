91587fbf78449f042990c17c1a9b891588854346
Mare <sevilamare@gmail.com>
2020-10-22 17:49:51 -0300

    adjust seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@
   Measure.create(name: item)
 end
 
-# Mock user
-User.create(username: 'test', email: 'test@email.com', password_digest: 'test123')
+# Mock user - ps field send as password and store as password_digest due bcrypt
+User.create(username: 'test', email: 'test@email.com', password: 'test123')
 
 # Mock measurements
-User.first.measurements.create(value: 123, measure_id: 1)
+# User.first.measurements.create(value: 123, measure_id: 1)
