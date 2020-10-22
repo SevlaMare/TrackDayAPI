9a35b78841187b653020d6b052853e88ff04569e
Mare <sevilamare@gmail.com>
2020-10-22 17:28:37 -0300

    login control

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
-
-  # LOGIN
   def create
-    if (userx = User.find_by(name: params[:name]))
+    @user = User.find_by(username: params[:username])
 
-      cookies[:current_user_id] = {
-        value: userx.id, expires: Time.now + 7200
-      }
-
-      render json: @user, status: :ok
-
+    if @user && @user.authenticate(params[:password])
+      secret = Rails.application.secrets.secret_key_base
+      token = JWT.encode({ user_id: @user.id }, secret, 'HS256')
+      render json: { user: @user, token: token }
     else
-      render json: @users, status: :bad_request
+      render json: { error: 'Invalid Username or Password' }, status: 401
     end
   end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  # ONLY WITH COOKIE
   def current_user
     current_user = User.find_by(id: cookies[:current_user_id])
     # current_user = User.first
@@ -8,5 +9,34 @@ class ApplicationController < ActionController::API
 
   def require_login
     render json: @users, status: :bad_request unless current_user
+  end
+
+
+  # TRY JWT JOKEN
+
+  # payload will be user_id
+  def encode_JWT(payload, secret)
+    SECRET = Rails.application.secrets.secret_key_base
+    # SPAM = 16.hours.from_now
+    # payload[:exp] = SPAM.to_i
+
+    return JWT.encode(payload, SECRET, 'HS256')
+  end
+
+  def decode_JWT(token)
+    SECRET = Rails.application.secrets.secret_key_base
+    body = JWT.decode(token, SECRET, )[0]
+
+    # ao inves de token como arg
+    # token = auth_header.split(' ')[1]
+    # header: {'Authorization': 'Bearer <token>'}
+
+    begin
+      JWT.decode(body, SECRET, true, algorithm: 'HS256')
+    rescue JWT::DecodeError
+      # expiration or invalid hash
+      nil
+    end
+
   end
 end
