5602bd5babc7fa68ef3cc65dc41be96c02f099d6
Mare <sevilamare@gmail.com>
2020-10-23 11:44:56 -0300

    allow user retrieve own data

--- app/controllers/api/v1/measurements_controller.rb
+++ app/controllers/api/v1/measurements_controller.rb
@@ -1,29 +1,32 @@
 class Api::V1::MeasurementsController < ApplicationController
-  # before_action :require_login, only: %i[index create]
-
+  # restricted route
   def index
-    @measurements = current_user.measurements
+    @secret = Rails.application.secrets.secret_key_base
+    @token = request.headers['Authorization'].split(' ')[0]
+    @user_id = JWT.decode(@token, @secret, algorithm: 'HS256')[0]['user_id']
+    @current_user = User.find_by(id: @user_id)
 
-    render json: @measurements, status: :ok
+    if @current_user
+      @measurements = @current_user.measurements
+      render json: { data: @measurements }, status: :ok
+    end
   end
 
   # restricted route
   def create
     @secret = Rails.application.secrets.secret_key_base
     @token = request.headers['Authorization'].split(' ')[0]
-
     @user_id = JWT.decode(@token, @secret, algorithm: 'HS256')[0]['user_id']
-
     @current_user = User.find_by(id: @user_id)
-    if @current_user
 
+    if @current_user
       @body = JSON.parse(request.raw_post)
-      @measurement = @current_user.measurements.build(value: @body['value'], measure_id: @body['measure_id']) # @body[1] failing
+      @measurement = @current_user.measurements.build(value: @body['value'], measure_id: @body['measure_id'])
 
       if @measurement.save
         render json: { message: 'Measurement registered.' }, status: :ok
       else
-        render json: { message: 'Invalid'}, status: 406
+        render json: { message: 'Invalid' }, status: 406
       end
     else
       render json: { error: 'Need login first!' }, status: 401
