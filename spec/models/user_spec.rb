3cdd14bc77e2a33f31ac847c0793421ac2d3ecf7
Mare <sevilamare@gmail.com>
2020-11-05 17:12:38 -0300

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.new }
-  let(:user1) { User.create(name: 'first', email: 'first@email.com', password: 'abc123') }
+  let(:user1) { User.create(username: 'first', password: 'abc123', email: 'first@email.com') }
+  let(:user_invalid) { User.new(username: 'first', password: 'asd123') }
   let(:measurement1) { user1.measurements.create(value: 123, measure_id: 1) }
 
   context 'Model' do
@@ -10,12 +11,18 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_nil
     end
 
-    it 'create a user' do
+    it 'create a valid user' do
       user1
-      expect(User.first.name).not_to be_nil
+      expect(User.first.username).not_to be_nil
     end
 
-    it 'create a measurement' do
+    it 'forbid rewrite user' do
+      user1
+      user_invalid
+      expect(user_invalid.save).to eq(false)
+    end
+
+    it 'autofill id using association' do
       user1
       measurement1
       expect(user1.measurements.first).not_to be_nil
